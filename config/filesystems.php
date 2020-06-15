3a46a3cfaf165e73591e345504e8a8c89cc3bd68
Alexander Trunov <sasha@clicksco.com>
2020-06-15 19:16:58 +0300

    Described new disk to place raw json feed file.

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,11 @@ return [
             'visibility' => 'public',
         ],
 
+        'raw_json' => [
+            'driver' => 'local',
+            'root' => storage_path('raw')
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
