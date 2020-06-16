85b97df12f7ab13b8e9ea63ff60c315131e04501
Alexander Trunov <sasha@clicksco.com>
2020-06-16 13:39:25 +0300

    Added vuetify and additional packages. Initialize frontend part.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+import vuetify from "./vuetify";
+
+window.Vue = require('vue');
+
+Vue.component('articles-list', require('./components/ArticlesList.vue').default);
+
+const app = new Vue({
+    vuetify
+}).$mount('#app');
